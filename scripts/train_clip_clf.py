fbf6e003d92adaac5e1cb9d4ea8414117efc4a8b
Yoann Poupart <66315201+Xmaster6y@users.noreply.github.com>
2024-04-15 11:02:31 +0200

    CLIP classifier training (#11)
    * next version
    
    * pooler output dataset script
    
    * hf compatible clf training
    
    * trainable parameters
    
    * CLIPForImageClassification
    
    * main model
    
    * loss
    
    * base model
    
    * entity

--- scripts/train_clip_clf.py
+++ scripts/train_clip_clf.py
@@ -2,7 +2,7 @@
 
 Run with:
 ```
-poetry run python -m scripts.train_clf
+poetry run python -m scripts.train_clip_clf
 ```
 """
 
@@ -13,10 +13,9 @@ import wandb
 from datasets import Features, Image, Value, load_dataset
 from huggingface_hub import HfApi
 from torch.utils.data import DataLoader
-from transformers import CLIPModel, CLIPProcessor
+from transformers import AutoConfig, CLIPForImageClassification, CLIPProcessor
 
 from scripts.constants import ASSETS_FOLDER, HF_TOKEN, WANDB_API_KEY
-from scripts.utils.clf import CLF
 
 ####################
 # HYPERPARAMETERS
@@ -40,13 +39,6 @@ print(f"[INFO] Running on {DEVICE}")
 hf_api = HfApi(token=HF_TOKEN)
 wandb.login(key=WANDB_API_KEY)  # type: ignore
 
-processor = CLIPProcessor.from_pretrained(ARGS.model_name)
-model = CLIPModel.from_pretrained(ARGS.model_name)
-model.eval()
-model.to(DEVICE)
-for param in model.parameters():
-    param.requires_grad = False
-
 if ARGS.download_dataset:
     hf_api.snapshot_download(
         repo_id=ARGS.dataset_name,
@@ -58,6 +50,31 @@ dataset = load_dataset(
     f"{ASSETS_FOLDER}/{ARGS.dataset_name}", features=features
 )
 dataset = dataset.class_encode_column("class")
+class_feature = dataset["train"].features["class"]
+
+processor = CLIPProcessor.from_pretrained(ARGS.model_name)
+config = AutoConfig.from_pretrained(ARGS.model_name)
+config.problem_type = "single_label_classification"
+config.label2id = {
+    label: str(i) for i, label in enumerate(class_feature.names)
+}
+config.id2label = {
+    str(i): label for i, label in enumerate(class_feature.names)
+}
+config.num_labels = class_feature.num_classes
+
+model = CLIPForImageClassification.from_pretrained(
+    ARGS.model_name, config=config
+)
+model.to(DEVICE)
+trainable_parameter_names = []
+for name, param in model.named_parameters():
+    if "classifier" in name:
+        param.requires_grad = True
+        trainable_parameter_names.append(name)
+    else:
+        param.requires_grad = False
+print(f"[INFO] Trainable parameters: {trainable_parameter_names}")
 
 
 def collate_fn(batch):
@@ -78,21 +95,16 @@ val_dataloader = DataLoader(
     collate_fn=collate_fn,
 )
 
-clf = CLF(
-    n_hidden=model.vision_model.config.hidden_size,
-    classes=dataset["train"].features["class"].names,
-)
-clf.to(DEVICE)
-
 with wandb.init(  # type: ignore
-    project="mulsi-clf",
+    project="clip-clf",
+    entity="mulsi",
     config={
         **vars(ARGS),
     },
 ) as wandb_run:
-    optimizer = torch.optim.Adam(clf.parameters(), lr=ARGS.lr)
+    optimizer = torch.optim.Adam(model.parameters(), lr=ARGS.lr)
     for epoch in range(ARGS.n_epochs):
-        clf.train()
+        model.train()
         for i, batch in enumerate(train_dataloader):
             images, classes = batch
             image_inputs = processor(
@@ -102,13 +114,13 @@ with wandb.init(  # type: ignore
             image_inputs = {k: v.to(DEVICE) for k, v in image_inputs.items()}
             labels = torch.tensor(classes).to(DEVICE)
             optimizer.zero_grad()
-            x = model.vision_model(**image_inputs)
-            loss = clf.loss(x["pooler_output"], labels)
+            output = model(**image_inputs, labels=labels)
+            loss = output["loss"]
             loss.backward()
             optimizer.step()
-            wandb.log({"train_loss": loss.item()})
+            wandb.log({"train/loss": loss.item()})
 
-        clf.eval()
+        model.eval()
         with torch.no_grad():
             val_loss = 0
             for i, batch in enumerate(val_dataloader):
@@ -121,17 +133,12 @@ with wandb.init(  # type: ignore
                     k: v.to(DEVICE) for k, v in image_inputs.items()
                 }
                 labels = torch.tensor(classes).to(DEVICE)
-                x = model.vision_model(**image_inputs)
-                loss = clf.loss(x["pooler_output"], labels)
+                output = model(**image_inputs, labels=labels)
+                loss = output["loss"]
                 val_loss += loss.item()
             val_loss /= len(val_dataloader)
-            wandb.log({"val_loss": val_loss})
+            wandb.log({"val/loss": val_loss})
 
-    torch.save(clf.state_dict(), f"{ASSETS_FOLDER}/model.pt")
-
-hf_api.upload_file(
-    path_or_fileobj=f"{ASSETS_FOLDER}/model.pt",
-    path_in_repo=f"data/{ARGS.model_name}",
-    repo_id=ARGS.dataset_name.replace("concepts", "clfs"),
-    repo_type="dataset",
+model.push_to_hub(
+    ARGS.dataset_name.replace("concepts", ARGS.model_name.split("/")[-1]),
 )
