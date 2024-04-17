32137742fc2e6b9266e4a82eef40c26ab93b5b9c
Imene Kerboua <imene.kerboua@esker.com>
2024-04-17 13:05:29 +0200

    fix script formatting

--- scripts/logistic_regression_clf.py
+++ scripts/logistic_regression_clf.py
@@ -1,10 +1,10 @@
-import os
 import argparse
+import os
 
-from loguru import logger
-from datasets import load_dataset
-from sklearn.linear_model import LogisticRegression
 import joblib
+from datasets import load_dataset
+from loguru import logger
+from sklearn.linear_model import LogisticRegression
 
 
 def main(args):
@@ -12,21 +12,15 @@ def main(args):
     dataset = load_dataset(args.dataset_name)
     dataset = dataset.class_encode_column("class")
 
-    train_ds = dataset['train']
-    test_ds = dataset['test']
+    train_ds = dataset["train"]
+    test_ds = dataset["test"]
     logger.info(f"Train shape: {train_ds.shape}, Test shape: {test_ds.shape}")
 
     logger.info(f"Train LR classifier for max {args.max_iter} iterations")
     lr_clf = LogisticRegression(max_iter=args.max_iter)
-    lr_clf.fit(
-        X=train_ds['pooler'],
-        y=train_ds['class']
-    )
+    lr_clf.fit(X=train_ds["pooler"], y=train_ds["class"])
 
-    score = lr_clf.score(
-        X=test_ds['pooler'],
-        y=test_ds['class']
-    )
+    score = lr_clf.score(X=test_ds["pooler"], y=test_ds["class"])
     logger.info(f"Accuracy score in test set: {score}")
 
     logger.info(f"Save model to {args.output_dir}")
@@ -38,9 +32,7 @@ def main(args):
 def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "--dataset_name", 
-        type=str, 
-        default="mulsi/fruit-vegetable-pooler"
+        "--dataset_name", type=str, default="mulsi/fruit-vegetable-pooler"
     )
     parser.add_argument("--max_iter", type=int, default=300)
     parser.add_argument("--output_dir", type=str, default="outputs")
