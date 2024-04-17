77af76682a891f13113adb5e9f4fc62313fa524a
Xmaster6y <66315201+Xmaster6y@users.noreply.github.com>
2024-04-17 10:33:06 +0200

    name n info refacto

--- scripts/make_pooler_dataset.py
+++ scripts/make_pooler_dataset.py
@@ -6,18 +6,16 @@ poetry run python -m scripts.make_pooler_dataset
 ```
 """
 
-import sys
 import argparse
 import logging
-import re
+import sys
 
 import torch
 from datasets import Dataset, DatasetDict, load_dataset
-from huggingface_hub import HfApi
 from torch.utils.data import DataLoader
-from transformers import CLIPVisionModel, CLIPImageProcessor
+from transformers import CLIPImageProcessor, CLIPVisionModel
 
-from scripts.constants import ASSETS_FOLDER, HF_TOKEN
+from scripts.constants import HF_TOKEN
 
 logging.basicConfig(
     stream=sys.stdout,
@@ -29,20 +27,21 @@ DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 
 
 def collate_fn(batch):
-    images, labels = [], []
+    images, infos = [], []
     for x in batch:
         images.append(x.pop("image"))
-        labels.append(x.pop("class"))
-    return images, labels
+        x.pop("original_name")
+        infos.append(x)
+    return images, infos
 
 
 def make_batch_gen(
     batched_pooler,
-    labels,
+    infos,
 ):
     def gen():
-        for pooler, label in zip(batched_pooler, labels):
-            yield {"pooler": pooler.detach().float().numpy(), "labels": label}
+        for pooler, info in zip(batched_pooler, infos):
+            yield {"pooler": pooler.detach().float().numpy(), **info}
 
     return gen
 
@@ -54,37 +53,25 @@ def gen_pooler_from_model(
     dataloader,
 ):
     for batch in dataloader:
-        images, labels = batch
+        images, infos = batch
         image_inputs = processor(
             images=images,
             return_tensors="pt",
         )
         image_inputs = {k: v.to(DEVICE) for k, v in image_inputs.items()}
         out = model(**image_inputs)
-        yield out['pooler_output'].detach().float().numpy(), labels
+        yield out["pooler_output"].detach().float().numpy(), infos
 
 
 def main(args: argparse.Namespace):
     logging.info(f"Running on {DEVICE}")
-    hf_api = HfApi(token=HF_TOKEN)
 
     processor = CLIPImageProcessor.from_pretrained(args.model_name)
     model = CLIPVisionModel.from_pretrained(args.model_name)
     model.eval()
     model.to(DEVICE)
 
-    if args.download_dataset:
-        hf_api.snapshot_download(
-            repo_id=args.dataset_name,
-            repo_type="dataset",
-            local_dir=f"{ASSETS_FOLDER}/{args.dataset_name}",
-            revision="refs/convert/parquet"
-        )
-
-    dataset = load_dataset(
-        args.dataset_name, 
-        revision="refs/convert/parquet"
-    )
+    dataset = load_dataset(args.dataset_name, revision="refs/convert/parquet")
     logging.info(f"Loaded dataset: {dataset}")
 
     splits = ["train", "validation", "test"]
@@ -96,26 +83,32 @@ def main(args: argparse.Namespace):
         )
         for split in splits
     }
-    
+
     dataset_dict = {
-        f"pooler_output": DatasetDict(
+        "pooler_output": DatasetDict(
             {
-                split: Dataset.from_generator(gen_pooler_from_model, 
-                                              gen_kwargs={
-                                                  "processor": processor, 
-                                                  "model": model, 
-                                                  "dataloader": dataloaders[split]
-                                                })
+                split: Dataset.from_generator(
+                    gen_pooler_from_model,
+                    gen_kwargs={
+                        "processor": processor,
+                        "model": model,
+                        "dataloader": dataloaders[split],
+                    },
+                )
                 for split in splits
             }
         )
     }
 
-    for dataset in dataset_dict.items():
-        # dataset.push_to_hub(
-        #     repo_id=args.output_dataset_name
-        # )
-        print(dataset)
+    for config_name, dataset in dataset_dict.items():
+        if args.push_to_hub:
+            dataset.push_to_hub(
+                repo_id=args.output_dataset_name,
+                config_name=config_name,
+                token=HF_TOKEN,
+            )
+        else:
+            logging.info(f"Dataset {config_name}: {dataset}")
 
 
 def parse_args() -> argparse.Namespace:
@@ -126,15 +119,18 @@ def parse_args() -> argparse.Namespace:
     parser.add_argument(
         "--dataset_name",
         type=str,
-        default="Xmaster6y/fruit-vegetable-concepts",
+        default="mulsi/fruit-vegetable-concepts",
     )
     parser.add_argument(
         "--output_dataset_name",
         type=str,
-        default="mulsi/fruit-vegetable-pooler"
+        default="mulsi/fruit-vegetable-pooler",
     )
     parser.add_argument(
-        "--download_dataset", action="store_true", default=False
+        "--download_dataset", argparse.BooleanOptionalAction, default=False
+    )
+    parser.add_argument(
+        "--push_to_hub", argparse.BooleanOptionalAction, default=False
     )
     parser.add_argument("--batch_size", type=int, default=64)
     return parser.parse_args()
