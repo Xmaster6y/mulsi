a0525c8a97ef05dfd7c0c6a4bce70a9d39e01199
Imene Kerboua <imene.kerboua@esker.com>
2024-04-30 12:39:37 +0200

    add script for pre labeling datasets

--- scripts/constants.py
+++ scripts/constants.py
@@ -8,3 +8,15 @@ HF_TOKEN = os.getenv("HF_TOKEN")
 WANDB_API_KEY = os.getenv("WANDB_API_KEY")
 
 ASSETS_FOLDER = pathlib.Path(__file__).parent / "assets"
+
+SPLITS = ["train", "test"]
+DATASET_NAME = "mulsi/fruit-vegetable-concepts"
+CLASSES = [
+    'cucumber', 'ginger', 'jalepeno', 'mango', 'orange', 'eggplant',
+    'cauliflower', 'tomato', 'kiwi', 'peas', 'potato', 'lemon',
+    'chilli pepper', 'watermelon', 'apple', 'lettuce', 'banana',
+    'corn', 'cabbage', 'capsicum', 'spinach', 'garlic', 'soy beans',
+    'grapes', 'carrot', 'paprika', 'beetroot', 'turnip', 'pineapple',
+    'bell pepper', 'raddish', 'onion', 'pear', 'pomegranate',
+    'sweetcorn', 'sweetpotato'
+]
