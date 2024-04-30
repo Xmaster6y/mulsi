df2fbc9eef78df2730739da902d5ea7c5318b83b
Imene Kerboua <imene.kerboua@esker.com>
2024-04-30 12:51:30 +0200

    add linting

--- scripts/pre_labeling_dataset.py
+++ scripts/pre_labeling_dataset.py
@@ -1,55 +1,62 @@
 """Script for pre-labeling the dataset.
 """
+
 import argparse
 
-from huggingface_hub import HfApi
 import jsonlines
 import pandas as pd
+from huggingface_hub import HfApi
 from loguru import logger
 
-from scripts.constants import ASSETS_FOLDER, HF_TOKEN, DATASET_NAME, SPLITS, CLASSES
-
+from scripts.constants import (
+    ASSETS_FOLDER,
+    CLASSES,
+    DATASET_NAME,
+    HF_TOKEN,
+    SPLITS,
+)
 
 CLASS_CONCEPTS_VALUES = {
-    'cucumber': ['green', 'cylinder'],
-    'ginger': ['brown'],
-    'jalepeno': ['stem', 'green', 'cylinder'],
-    'mango': ['seed', 'orange', 'ovaloid'],
-    'orange': ['orange', 'sphere'],
-    'eggplant': ['purple', 'cylinder'],
-    'cauliflower': ['white'],
-    'tomato': [], # already labeled
-    'kiwi': [], # already labeled
-    'peas': ['seed', 'green', 'sphere'],
-    'potato': ['brown', 'ovaloid'],
-    'lemon': [], # already labeled
-    'chilli pepper': ['red'],
-    'watermelon': [], # already labeled
-    'apple': ['red', 'green'],
-    'lettuce': ['leaf', 'green'],
-    'banana': ['yellow', 'cylinder'],
-    'corn': ['seed', 'yellow'],
-    'cabbage': ['green'],
-    'capsicum': ['green'],
-    'spinach': ['leaf', 'green'],
-    'garlic': ['white'],
-    'soy beans': ['seed', 'brown'],
-    'grapes': ['green'],
-    'carrot': ['orange', 'stem'],
-    'paprika': ['red', 'stem'],
-    'beetroot': ['red', 'stem', 'tail'],
-    'turnip': ['white', 'purple', 'stem'],
-    'pineapple': ['ovaloid', 'brown', 'yellow'],
-    'bell pepper': ['green', 'red', 'yellow', 'stem'],
-    'raddish': ['red', 'stem'],
-    'onion': ['white', 'sphere'],
-    'pear': ['green'],
-    'pomegranate': ['red', 'seed'],
-    'sweetcorn': ['seed', 'yellow'],
-    'sweetpotato': ['orange', 'brown', 'ovaloid']
+    "cucumber": ["green", "cylinder"],
+    "ginger": ["brown"],
+    "jalepeno": ["stem", "green", "cylinder"],
+    "mango": ["seed", "orange", "ovaloid"],
+    "orange": ["orange", "sphere"],
+    "eggplant": ["purple", "cylinder"],
+    "cauliflower": ["white"],
+    "tomato": [],  # already labeled
+    "kiwi": [],  # already labeled
+    "peas": ["seed", "green", "sphere"],
+    "potato": ["brown", "ovaloid"],
+    "lemon": [],  # already labeled
+    "chilli pepper": ["red"],
+    "watermelon": [],  # already labeled
+    "apple": ["red", "green"],
+    "lettuce": ["leaf", "green"],
+    "banana": ["yellow", "cylinder"],
+    "corn": ["seed", "yellow"],
+    "cabbage": ["green"],
+    "capsicum": ["green"],
+    "spinach": ["leaf", "green"],
+    "garlic": ["white"],
+    "soy beans": ["seed", "brown"],
+    "grapes": ["green"],
+    "carrot": ["orange", "stem"],
+    "paprika": ["red", "stem"],
+    "beetroot": ["red", "stem", "tail"],
+    "turnip": ["white", "purple", "stem"],
+    "pineapple": ["ovaloid", "brown", "yellow"],
+    "bell pepper": ["green", "red", "yellow", "stem"],
+    "raddish": ["red", "stem"],
+    "onion": ["white", "sphere"],
+    "pear": ["green"],
+    "pomegranate": ["red", "seed"],
+    "sweetcorn": ["seed", "yellow"],
+    "sweetpotato": ["orange", "brown", "ovaloid"],
 }
 
 assert len(CLASSES) == len(CLASS_CONCEPTS_VALUES.keys())
+
 
 def get_metadata(hf_api: HfApi, split: str):
     metadata = []
@@ -61,16 +68,22 @@ def get_metadata(hf_api: HfApi, split: str):
         local_dir=f"{ASSETS_FOLDER}/{DATASET_NAME}",
     )
 
-    with jsonlines.open(f"{ASSETS_FOLDER}/{DATASET_NAME}/data/{split}/metadata.jsonl") as reader:
+    with jsonlines.open(
+        f"{ASSETS_FOLDER}/{DATASET_NAME}/data/{split}/metadata.jsonl"
+    ) as reader:
         for row in reader:
             metadata.append(row)
     return metadata
 
 
-def save_metadata(hf_api: HfApi, metadata: dict, split: str, push_to_hub: bool = False):
-    with jsonlines.open(f"{ASSETS_FOLDER}/{DATASET_NAME}/data/{split}/metadata.jsonl", mode='w') as writer:
+def save_metadata(
+    hf_api: HfApi, metadata: dict, split: str, push_to_hub: bool = False
+):
+    with jsonlines.open(
+        f"{ASSETS_FOLDER}/{DATASET_NAME}/data/{split}/metadata.jsonl", mode="w"
+    ) as writer:
         writer.write_all(metadata)
-    
+
     if push_to_hub:
         hf_api.upload_file(
             path_or_fileobj=f"{ASSETS_FOLDER}/{DATASET_NAME}/data/{split}/metadata.jsonl",
@@ -89,7 +102,7 @@ def main(args):
         logger.info("Pre-label concepts")
         df = pd.DataFrame.from_records(metadata)
 
-        assert len(df["class"].unique()) == len(CLASS_CONCEPTS_VALUES.keys()) 
+        assert len(df["class"].unique()) == len(CLASS_CONCEPTS_VALUES.keys())
 
         for key, values in CLASS_CONCEPTS_VALUES.items():
             for val in values:
